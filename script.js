949ad3564187e160dc2bae17fdef8399c2ce2535
Alen Abrahamyan <alenabrahamyan0@gmail.com>
2022-10-19 03:14:39 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const searchInput = document.getElementsByClassName("search-input")[0]
+const typingWords = ["Data Structures", "JavaScript", "DevOps"]
+
+let word = 0,
+  letter = 0,
+  diff = 1
+
+setInterval(() => {
+  word = word % typingWords.length
+  searchInput.placeholder = typingWords[word].slice(0, letter) + "|"
+
+  letter += diff
+
+  if (letter >= typingWords[word].length) {
+    diff = -1
+  } else if (letter <= 0) {
+    diff = 1
+    letter = 0
+    word++
+  }
+}, 1000 / 6)
