61c4832a21cf291c667c06a8d03f1148739bd955
JSHayk <jshaykdeveloper@gmail.com>
2023-03-07 18:03:46 +0400

    added local data

---
+++ data/geeks/index.js
@@ -0,0 +1,302 @@
+window.geeks = [
+    {
+        name: "Hovhannes Dallakyan",
+        hashtags: [
+            "JavaScript",
+            "HTML/CSS",
+            "Frontend"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/C3xv_IGT2bmTRowTAWk2eg/IA34xL2eTlM8a6cdyPy736wiPiHPTVaaYIkTfNJDDRwWidqOaaNbZ7z5OfTpCFfQRhULfkNeaWQLpjv2yeddIA/-qFC5rODmKPKBY_p-9UN32wnGROzsQvK_gClxfmWNQo",
+        websites: {
+            "youtube": "https://www.youtube.com/channel/UCQbztcEfX0PgUhbTrFnyBIA/",
+            "telegram": "https://t.me/dallakyanlab",
+            "facebook": "https://www.facebook.com/hdallakyan"
+        }
+    },
+    {
+        name: "Anushavan Yeghiazaryan",
+        hashtags: [
+            "JavaScript",
+            "React",
+            "HTML/CSS"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/VntUgcMimG4nCTkobVTVNQ/s8Li0fVEs4wBsPxauBQgpZqok8V4dj6IxJdhQdVb4BQpZzdg3z8mXN4IEnWGadcTeS8PEE3rEEgefgpuWTFHvg/mCJ_NmkgeymFn4-7MznDX_mMEeFt1tEZHigLQw3d5ic",
+        websites: {
+            "youtube": "https://www.youtube.com/c/AnushavanYeghiazaryan",
+            "facebook": "https://www.facebook.com/anushavan.yeghiazaryan24"
+        }
+    },
+    {
+        name: "Nairi Harutyunyan",
+        hashtags: [
+            "JavaScript",
+            "Node.js",
+            "Interviews",
+            "Podcasts"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/VZiZZ6TTHRyg6X2EbAI0NQ/aG9Uj28W7xHtrKqEK10QtMOX-hiACYEpWP0C0d1hEOI0XlaNBPBbDo3ENFFpDVPCFWM4Fk6A3qDYBNV7P8O7rQ/jIDSf4JPQopb5DPxJwLJwf8_nPSSz2Rjk8wEDUS3pZU",
+        websites: {
+            "youtube": "https://www.youtube.com/c/nairihar",
+            "medium": "https://nairihar.medium.com/",
+            "facebook": "https://www.facebook.com/nairihar"
+        }
+    },
+    {
+        name: "Rouben Meschian",
+        hashtags: [
+            "JavaScript",
+            "React",
+            "Node.js",
+            "Databases"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/wUJ1qYrFUfuA28mJ8A_bcQ/nuz-qWlFWBsv0HfVc6OhoAOOKzfD20VZgazGIuIqgXZq92sHAGFlYiDP5zZSFqxCKW6MzLo750kKDW1g2egO5w/m37GA1Cglw8631sNol2KfKcyiy6SxG6ehPLVkYhMysA",
+        websites: {
+            "youtube": "https://www.youtube.com/user/meschian",
+            "facebook": "https://www.facebook.com/meschian/"
+        }
+    },
+    {
+        name: "Alen Abrahamyan",
+        hashtags: [
+            "Canvas",
+            "JavaScript"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/vZhBHrNB0pJKQHn88YaK5g/plhbktBQdqh6XB0d0BNCw2F1d9Um4zjCVIxou3cia12GPtRGSGg6ZKhYbufLNFMCHdoNnHgKYUKbPD_2eMMqPw/vKRRHEdtrHnMTtpzs-zS_cLGzEU1qKALiczy54K3wiM",
+        websites: {
+            "youtube": "https://www.youtube.com/channel/UC3wj8tB4qIDH3u8_MHfbN8w",
+            "medium": "https://medium.com/@AlenAbrahamyan",
+            "facebook": "https://www.facebook.com/alen.abrahamyan.71"
+        }
+    },
+    {
+        name: "Tigran Hayrapetyan",
+        hashtags: [
+            "Algorithms",
+            "Data structures"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/7LCvWIoO836TNTi3ibUSXg/5NJllTWBuIIx294WiFsPTDU5qis4gVZr-CWmWHXP9r8kxhy7HJ6SnpETdY2scNv-nN8AsKYLl7TBhTxRMWE1Hg/mhAxmYEST-atBnVC80c1qKhoC-OnT8YJiDo6NZRL_pI",
+        websites: {
+            "youtube": "https://www.youtube.com/channel/UCyW2YneDEtZj3QMTbsxKj9g"
+        }
+    },
+    {
+        name: "Davit Baghdasaryan",
+        hashtags: [
+            "Podcasts"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/GEVP2vR8iTt81JD_V_2kww/CyRMSc4tujFhk4ZfGzJMIkyVMBokrJh2FRuQZ0C90tt9-E_nUefgtmofovh7x2QiWHssz195aaQwGlqmoK8YZA/1LHj0JYXQ8tE8fz9FeF_dIUNmi76_E2CjFjKNPNG7Ww",
+        websites: {
+            "youtube": "https://www.youtube.com/channel/UCzj2s7KYMpStMmnanYHkDmw",
+            "facebook": "https://www.facebook.com/dbaghdasaryan"
+        }
+    },
+    {
+        name: "Narek Safaryan",
+        hashtags: [
+            "Startups"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/KU6OH79n4soVYjzEE4qPwQ/1DXJ7-7J805zCcXFwy4Gcznl_j0ajBvUiwJnA-Lbk0Upzy8S9TLpFiJZhiae9MpSFXHjECmC_bs3xUFzqd-ozw/y9KTi9KA7DhmVOpfgdrAojASaaQCTwHiWCZo5yoVTW8",
+        websites: {
+            "youtube": "https://www.youtube.com/c/StartupGrowthArmenia/",
+            "facebook": "https://www.facebook.com/ArmSnowboarder"
+        }
+    },
+    {
+        name: "Arthur Muradyan",
+        hashtags: [
+            "Machine Learning",
+            "Podcasts"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/lkbZHwuMCoOqF_KWusCZDw/4qGFqhZmN5eb07yTAb0z49weelfZpE_2T7XS14aVAVFwhF5dyjsprzXu3xIXJe0ojbxNJ42SL2BBtYpU4GAerg/aE_N0BYw1LXfFwxMEzDIcDfjVtIzuhLQLChLxjVdDP0",
+        websites: {
+            "youtube": "https://www.youtube.com/user/ArthurMuradyan"
+        }
+    },
+    {
+        name: "Sami Hayrapetyan",
+        hashtags: [
+            "HTML/CSS",
+            "Git"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/4dKZ26EVvWErOrHmRElevw/VTaEdpzGAkW9dML4nRTLK0vj8HoRNnl-OqvHiyg9GAe_N3rCfooo0xPBCCamnFMCSIkvkBfvQYZQJU9gVaBoBw/39uFS310WHGi0gI2oJ6cLByVubsu7zMN47DmqsiPh-o",
+        websites: {
+            "youtube": "https://www.youtube.com/c/SamiHayrapetyan",
+            "facebook": "https://www.facebook.com/SamiHayrapetyanOfficial"
+        }
+    },
+    {
+        name: "Narine Poghosyan",
+        hashtags: [
+            "QA"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/UHeTg_3-4lZZWI6zV72Jdw/HX0E0Yp8ybLUFr9ttQT-24j8Z93tacAJ_RTRVnG4z9iXofSU8aqbLAz9oy1H60jBJr6m5bSMVzMGxQmzHJA-qg/bBQtD-aMR2Bv69hLQBeljdxGxFrEB3KFYjY1lk2RwHI",
+        websites: {
+            "youtube": "https://www.youtube.com/user/HayNariko",
+            "facebook": "https://www.facebook.com/narinepoghosyan"
+        }
+    },
+    {
+        name: "Seyran Tadevosyan",
+        hashtags: [
+            "Java",
+            "C-sharp",
+            "SQL"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/9gLv1OMVgxLPbgRVN_-ksg/1tltTCuxWU6lttIOgrtwTwGXzpYh6DLYa1V2SP6ibF8zbaafwllyoP2fDAzBZoJsp9m5ViS2rkZLsuBVOyxydA/VHY7kM6-UYTOIb878gXmEawkgGni__0e4O0eXZJ4bNA",
+        websites: {
+            "youtube": "https://www.youtube.com/c/NightCodeLive/playlists",
+            "facebook": "https://www.facebook.com/seyran.tadevosyan.967"
+        }
+    },
+    {
+        name: "Grigor Yeghiazaryan",
+        hashtags: [
+            "DevOps"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/JTbMLVpG3vpUWP7cSBj1gw/6AKFYeNtxC_22uFJk9wtJSQLh8EA9CQkiWgz274pc0JFrbm_3YFXEePVqGp5WD5dWaBI_myk72O4IVcy2B8ZuQ/AYzGv4R6LEUj2kHO4s8Qyte1NkFrOIIzqMNz3UNZsy0",
+        websites: {
+            "twitter": "https://twitter.com/grig_tech",
+            "youtube": "https://www.youtube.com/c/GrigTech",
+            "facebook": "https://www.facebook.com/grigorye"
+        }
+    },
+    {
+        name: "Andranik Sargsyan",
+        hashtags: [
+            "HTML/CSS",
+            "JavaScript"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/-Gfqhk2VwkXiYJQeq1isug/9ZEcgQ7Yc5MN_8fPW00oCv1aCL5_8NOK244qpDhbViClSaiJfPyNkvHmS_pHI9jvJOmGM9AXJbscA1NiMHaDdQ/yrk8u2ZuA6aB2dL2vo4CxpgEDoOSdwNfrKdhJixzLvM",
+        websites: {
+            "youtube": "https://www.youtube.com/channel/UCZXbYRualdbFzeNLPzBfjcQ",
+            "facebook": "https://www.facebook.com/crueland"
+        }
+    },
+    {
+        name: "Amur Khoyetsyan",
+        hashtags: [
+            "HTML/CSS"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/H2QL05q3Leam1WksHjj0Vw/lXJG3ZEU_SqF030vBGS_wPceG_GKivoo7vFy1vaxUgQvmxhRARXNycWU-qR0hbmARKOTeKoA3gIaizB0oNNEtw/Fx2h-FD1eaRic8L-AzI2bs7J-IcCpbjjnxVx4xtFM-k",
+        websites: {
+            "youtube": "https://www.youtube.com/c/AmurKhoyetsyan",
+            "facebook": "https://www.facebook.com/AmurKhoyetsyan"
+        }
+    },
+    {
+        name: "Arman Avetisyan",
+        hashtags: [
+            "Python",
+            "Algorithms",
+            "Data structures"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/jL-sO4Qnptnt5XhrAQI5Qw/Z2psD5pEXgPuxKeQcsMRvoEqg4vveA5ICnqEU62y4Jo1_gbHSZQdix6OZumfIvbPQztY51H1GbGVmIhL0qlwkg/KtJZCqlrn43UXZjkLbBiA-2l6rQJ_zqfPV49AF1K340",
+        websites: {
+            "youtube": "https://www.youtube.com/c/CodeTubes",
+            "facebook": "https://www.facebook.com/codetubesam/"
+        }
+    },
+    {
+        name: "Edgar Marukyan",
+        hashtags: [
+            "JavaScript",
+            "React"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/maJyyRecKMIzb9KzWlp3Ug/LW9vPdSp-VxP-12JuQuW9pqzg5ycd8q-gTy2MGK6LPF1DPiWN-AqvJEiakCepuROoVv6aSukS7TdTMUG-BR4vw/VUqUMCeQBOl0z_Z045o86_9R2Eq_r7aum3FqoIgav-A",
+        websites: {
+            "youtube": "https://www.youtube.com/channel/UCtdhQ5dsnKkd85d80IOzUvg",
+            "facebook": "https://www.facebook.com/edd.marukyan.7"
+        }
+    },
+    {
+        name: "Kristine Armiyants ",
+        hashtags: [
+            "Programming"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/bfRky3fZewxo5W7J3KDSqw/nSZT_vUjkqW7aOswDZfPQmwQGo42UMj488eelCFir6czpPa3kSF5E2uY1FYWBaf2wur-lksy0TMvSTpK4YoSMQ/X6ul5KECoCqQh-8xzy7hKvmzohXgJ-eUS5CcPynehs0",
+        websites: {
+            "medium": "https://medium.com/@christine.armiyants"
+        }
+    },
+    {
+        name: "Hrant Khachatrian",
+        hashtags: [
+            "Machine Learning",
+            "GraphTheory"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/gsb6stmcZ_6-Xt-DFaOkng/V-KydU0cPAcAPa5beEY9j7WZNmxhUeIK4aceekqYqYOVu6CCQQe2mM2Dbnc0l4MG39lVp2aeV2HHuvo5mbMmaQ/pOxMLt2LFRmQWQf2vawf_ek_tUO1TPQ1junIS96VSPQ",
+        websites: {
+            "youtube": "https://www.youtube.com/@HrantKhachatrian",
+            "facebook": "https://www.facebook.com/hrant.khachatrian"
+        }
+    },
+    {
+        name: "Ashot Hovhannisyan ",
+        hashtags: [
+            "JavaScript"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/z-hTXBn69JGRTpMb074x5Q/b_SOMog2urmsBk9CULcIJQX8EepGPPEK8Wu0dVw3ZVjI-04_WhLGsrhqHbwh7DQ56DO2tqvfn71s1Y68j6Jb_A/pXE4i9goJlzWHkXM0D_cvo_0ulkTp1HcdsAFQwx9tR8",
+        websites: {
+            "youtube": "https://www.youtube.com/@iamashot",
+            "medium": "https://medium.com/@iamashot"
+        }
+    },
+    {
+        name: "Vahram Martirosyan",
+        hashtags: [
+            "Computer Science",
+            "Algorithms"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/wN2wznAW76_e2Lw1cEq9Jg/F6bLmSsyxH1iiCusMurAzJPBUqbPhTjcS-FteQFdIiQAzpWqYkn-kZpsm36A8ydOu3z3lUcRRcUFan-E3UitaA/n8SYY3w6x6ZJbsBxVQ6tddeixft_-Um0t-gdvWfvCLc",
+        websites: {
+            "youtube": "https://www.youtube.com/c/MutExSchool",
+            "facebook": "https://www.facebook.com/vahram.martirosyan.7"
+        }
+    },
+    {
+        name: "Levon Poghosyan",
+        hashtags: [
+            "Networking",
+            "Algorithms",
+            "Data structures"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/OXAfCxd-p2guPBeQtiVhCA/4LmVZ26y-gE_00MejWTZWvsmnE7alURv06y5blCQ1PnuqRACtKZx0kZT6VFF_SMk0l7nkFBPAyNIydELtBlXQA/ca0BU6dGoI0oHHoX7OOHb_xrDtnknTpqiMF_6eol6W8",
+        websites: {
+            "youtube": "https://www.youtube.com/@deimusmeister"
+        }
+    },
+    {
+        name: "Antranig Vartanian",
+        hashtags: [
+            "Unix",
+            "CyberSecurity"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/SFK3bhCSG08MLkNLIl4HkA/SELDrsLOY-vk6VHMjcm3Qdx2YvPxo77IcF9DCtiAskZ-3x1eURdqY9p3Wh3VB-4Q12jnRUChXENXOhHhbo2hSA/_EocuWLD902LXr2O8MOYD2xSiSgksAyhxAY4YfSGP-s",
+        websites: {
+            "website": "https://weblog.antranigv.am"
+        }
+    },
+    {
+        name: "Vahe Avetikyan",
+        hashtags: [
+            "Java",
+            "Programming"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/qJesd64ua8Tv9zUoeR4_yg/vQSmeLSXWI0Qi0hBQYkVA0VHIFYENMC3dj_YlqKUxF8w4Zd6ytQlThaiLO8WeFxtTLTD2xEfzwG7-xNTkGMQ_g/fC0lYnC6BpTJbgi6B-7CEm56RGVzAXJ3RaTafC02otU",
+        websites: {
+            "medium": "https://medium.com/@vahe-avetikyan"
+        }
+    },
+    {
+        name: "Narek Harutyunyan",
+        hashtags: [
+            "Scratch",
+            "Programming"
+        ],
+        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/j7Dq4c2X3Yv_ga9-3q0tmA/21oHmsS-Uw3Sj3PZCPulZCZtT0ilaluSTD-3qFEYLj5DV2Sa3669QIRO6RnNJU8itDcMSktBuzRizgxuKeIOVg/SbkePVmXjghcl7EUHaEXTFvPyj1Y9SR2sZAjt_gAtY0",
+        websites: {
+            "youtube": "https://www.youtube.com/@user-do6dv9tf5e"
+        }
+    },
+]
+            
---
+++ data/telegram-groups/index.js
@@ -0,0 +1,112 @@
+window.telegramGroups = [
+    {
+        "name": "JavaScript Armenia",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/fSe0d5ql0_AuKYJkWVrJ9g/KO7iS7lji3OKyTPeu89YB-IBl2uBYd0BwzQJneEK9MlC3dd0uHorJUIzGYVTiZQJQTLaXdJFYkEQumJV85d_oA/GiDXub0j56CirP4XMb64XHqci-FLOJFJf3HD-02XhMA",
+        "link": "https://t.me/javascriptarmenia"
+    },
+    {
+        "name": "Node.js Armenia",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/3DTWZgVqtZ7aaeOC22gT0w/BdKYNqWdicIaqZuwzgUFevks9LNgEMF35yMxsOdJG61QqCv_3y-uTOfZRa-Rb5zxBdUuKu90-zzNixAOQkfs3Q/PbPOy53paby5RYHC6KVhIygjTwLK7I5ATQBFuFFjRy8",
+        "link": "https://t.me/nodejsarmenia"
+    },
+    {
+        "name": "React Armenia",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/W2Z7feF2RSptdYymLxgCrg/-mjRcDaHvB0BB3Uj2y4Md9l_mnZooddYcIxYx4US5RG5o2VEpCc5m-Vx2JvbESoHLuBraTUt0x1Re6MYjq3rmA/PCt7R9lhRZKtk4r5OuCD0TT92RFKEJFQ9txxhm1Wu7M",
+        "link": "https://t.me/reactarmenia"
+    },
+    {
+        "name": "Vue.js Armenia",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/zkN7I11i5udFSgIj0c-vDg/-vrfjH_8ZlMjVKXOohpLx0yPwtb3yus7eyzpVc8JIYxA7vMe00npJEOAfP_28a9K0_KW0agv2wS7Ldpnu-9-Rw/mJ-tHkj0JAynCAyPE8TA_znWjb-WmHwsO-gLxmn0XeA",
+        "link": "https://t.me/vuejsarmenia"
+    },
+    {
+        "name": "C++ Armenia",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/_om2YKWpMPY2MbW_-0o7fQ/slsULh93cIsI2-1QAdkLjvkZvLmyrTa-CC0Ywcw3RgRLODTvQB6-kGyEnmY-GNTiSn9TXFxiq-uQ-VnLQ9X0FA/P6WkcQ65wQDxDh9kjp39ABmjb83GxnDjF9e-TYtG1jw",
+        "link": "https://t.me/cpparm"
+    },
+    {
+        "name": "Juniors",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/Vl4Kwxi3AUmOACdkyRMbiw/N2xgP4jub5oyylU5UuVyx34UCWRvIT4IF3v9yCCSlZGuDeLW-2jm3Y251At538lSi1DGzyo72QHzLUXy7xugXA/T3M-aW5vTNRbgncVGm_PxgLV42X92m8Irg8bwi5g-Bw",
+        "link": "https://t.me/techjuniorsAMChat"
+    },
+    {
+        "name": "SwiftCoders Armenia",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/r8PXGjtn1PIuJ-9GC1Dwzw/q4wlIZCwn-F22IfQGe501bYSdUpGt2ZWSHsaRFCcFIEp0HLY3R_pz_G9TYu_FZjK_bwL0ITVXntl9xE5yBK0MA/wwbyPpGgskhS1UdGP2xDiyJGPuE1d2q8x3jdGElR9GM",
+        "link": "https://t.me/swiftcodersarmenia"
+    },
+    {
+        "name": "Java Armenia",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/v170nEpReepAi98hIy4gvg/yg81JGCJujD1F_aGqRje33luvcqDoYVZ2OF3hEDtDVKpFZHdjjxQh_Vh_Z43csQe8UrSamKAOwe9_LTRAMaJwA/LAO23yCNvbg3hHD2U4RYkl42UqhCWnLK1f_T0CGvpPE",
+        "link": "https://t.me/java_armenia"
+    },
+    {
+        "name": "Bēhance Armenia",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/p9GSlkf3DK8rmUdcQekeLw/Fxxm1SvQ5028BOJHq5mwihNKIUKLYhj3wKPae3oJMhFYTlL37S-IKWgEdWHecBbTr-rpbomsRloUazYyDuaVcQ/Czj3auRX0JQNAWhD5yT1U14jZdW0-WvRiqbhwGzNbEg",
+        "link": "https://t.me/behancearmenia"
+    },
+    {
+        "name": "Netbythe.net",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/gQLmO8IijkLpXh8mQYeuyQ/HdhdDqeNOHfQkYrAEER0M7jM_1B_ruCY2GJ9aRo0cO3AB9bEbdNKicEQDuafnCXutL9E-Qc9udg3HuUtBreFBA/jkgwnDH9Uo7yJlq61FuJkKlI4nD_wjsb_xNRWB9ducU",
+        "link": "https://t.me/netbythenet"
+    },
+    {
+        "name": "Laravel Heroes",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/v8RXJQ35b6hAdUfBVxkYLQ/QKAMcVTig4qaPYTVxpOFy_toaSnesXTJ4prwbH1FH8sh26hj_ZAPYjZf_nSvhTjQ72Gr8IHVdlSjQbwpKx42rg/DVeSQiEijxd2DITVWGXsftCZIKnKK4lBzo5qIUcC7JM",
+        "link": "https://t.me/LaravelHeroes"
+    },
+    {
+        "name": "PYerevan",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/HXnCgJYEZpXKZckT6-UCyw/k9HvpTHDPZ6E3Jxngs5sGPeupBEpvRfXArcRbJh0n83lJf4Xq12rIVyMGt05hm-3EDMqLB5dCv4wEscflFfUCQ/IAGsjJQy4giMxNhFHzVLKnn7TJ-4TxiDDxz8HJq--Ac",
+        "link": "https://t.me/pyerevan"
+    },
+    {
+        "name": "UX/UI Armenia",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/u2F_VDJW97oNBemQlVrQUw/l_PYr00bqTX7F-nKGMV30NR9iNT-XXJxx0z7aXSK1Cs5nSc3Z_aoo7vHGRCBkTg-FmloPKk8g84hXe6ratHe4g/06VqcellsTZ0DXhwthPvk8eQlDnD_pru0NVLUpzEthc",
+        "link": "https://t.me/ux_ui_armenia"
+    },
+    {
+        "name": "Laravel Armenia",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/welWu2ZjLHgToM7jH1eh6Q/_SGIVR0yQoTuQVxYzapQNXiBR0P7F_4aPGcYzWK26sc3mIYBy-C5k_gyD17-CJ2uG3VbY2A6DmBd4sFIxK3o4w/ZoKviAn-jiCMEVlMxaGClTLN7TUlbkraRk-zJBQc_Lg",
+        "link": "https://t.me/laravel_armenia_chat"
+    },
+    {
+        "name": "PHP Armenia",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/32-DYvrImzkWBO-aClDUqQ/cPN5JqYjEgomCDRO_01Nc82ApwFin_6_wtjtVirvW7exPdGkTvyfVGAxx6peh1ot5WnCIik3y_4KL_MjV17hyQ/D7mZi8jql2tIt3xL-7OsXjU4WeInTCo3IDX2TThlFDA",
+        "link": "https://t.me/php_armenia"
+    },
+    {
+        "name": "CODE Info",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/YFLVJbBMGVEvfyR4sT5jmQ/BqT9dtXxpeQzTi3NBcMBO-0AQvWHH1775eaGUy3Cp6COvop9kTjz3sRumaQg9mszPTzfWbN36grMMUDzlaTmWQ/NVzuNt-8KpupIYSSU0VHHLYT7F108h-fuy-9_-1GLRY",
+        "link": "https://t.me/CODEInformation"
+    },
+    {
+        "name": "Dart & Flutter Armenia",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/ggIPO16MDEUwjYTv3_F_Rg/s61U4804fYfsbl-AgXpUoECEynl2nbg3rhL5VkojHQl4G6XxP2cjbQYXV8IswwsLULFdQSlbOxn6yEiTPDPKXQ/wTi2nc1oSWMiupHv97yLbVfHJJRFvKVGaE8yPfwmjpE",
+        "link": "https://t.me/flutterarmenia"
+    },
+    {
+        "name": "Developer's Library",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/EXHmAHkIm2wTvfIs3-bqHw/m78uMkDpjgAwWHpDRtvNr9u2rlVKf_bpEAkOMH3PlUOfakNc0LPoyaYSwXlvTGzcURDzNvzzPCRnfXSN775FHQ/CUzniMVNIKOV0y2q9-tABDlf20m1sG07HmI4BoD2blE",
+        "link": "https://t.me/Developer_Lib"
+    },
+    {
+        "name": "Rust Armenia",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/bqqGyUtGQJX414pBXHbzWA/7gD3zCA1FcHMcgLtU9T61h86GvagX1Fgr65Y38EkW0vBN-LSPp_ER-JkqrRbYRfUXCCa7LHMxFH5cg7Ga_W9Pg/3eUnCIQllTVIWedqPr_Eb2ivwFKQFWnStIpH1tBvdoU",
+        "link": "https://t.me/rustarmenia"
+    },
+    {
+        "name": "AWS Armenia",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/yRMUhK5tH7ZzuauAfvdZKg/FYbBzDCFW9lZBZRcjnlazzYpY3qc4jf3gTB1T10vf4z1NM4lcw7MqYU1Y96PGZ3fvIjAFeKTxdigtambhDrBsw/sICygnCr9wz2lsTaOR6GSES4cBgbIWfbW8PM_xdQFH4",
+        "link": "https://t.me/AWSUserGroupArmenia"
+    },
+    {
+        "name": "Armenian Android Developers",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/JXkFxH_EMzuAwsMIpGk2jw/Lmi0_QEgN4rYAKCQWfVxSCoGL8mBS0z9VOm-GLDfIHxs_G6OxNpWhHmlH-Qqhw01VEHi0IlTT9F369uLZGp0vA/t_S_QI55Fw5OoAhypclN6aZtYdcREhW2THgZcwUcv6g",
+        "link": "https://t.me/armenian_android_developers"
+    },
+    {
+        "name": "սարեան",
+        "avatar": "https://v5.airtableusercontent.com/v1/15/15/1678204800000/6lgCbTduCUTLYJT48Dn8NA/w7mkqk-YEyVr3ytFVf56uUC9GzlKYowkq7m4bjsodaY5LBrelh7fccSSR4HnKxYh9PrJ5O7v0D335963YJl4Jg/39j8pLYjXa5pMAVxTn3iORdESXV1J0BkYFn9CIRPI9Y",
+        "link": "https://t.me/sarian_netters"
+    }
+]
