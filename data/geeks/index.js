e37b2ae0ffc5383a2265d32c3fa39cacbc2735e8
JSHayk <jshaykdeveloper@gmail.com>
2023-03-08 23:45:44 +0400

    changed avatar url

--- data/geeks/index.js
+++ data/geeks/index.js
@@ -1,3 +1,5 @@
+const geeks_url = "https://topentol.sirv.com/geeker/geeks"
+
 window.geeks = [
     {
         name: "Hovhannes Dallakyan",
@@ -6,7 +8,7 @@ window.geeks = [
             "HTML/CSS",
             "Frontend"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/C3xv_IGT2bmTRowTAWk2eg/IA34xL2eTlM8a6cdyPy736wiPiHPTVaaYIkTfNJDDRwWidqOaaNbZ7z5OfTpCFfQRhULfkNeaWQLpjv2yeddIA/-qFC5rODmKPKBY_p-9UN32wnGROzsQvK_gClxfmWNQo",
+        avatar: `${geeks_url}/Hovhannes-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/channel/UCQbztcEfX0PgUhbTrFnyBIA/",
             "telegram": "https://t.me/dallakyanlab",
@@ -20,7 +22,7 @@ window.geeks = [
             "React",
             "HTML/CSS"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/VntUgcMimG4nCTkobVTVNQ/s8Li0fVEs4wBsPxauBQgpZqok8V4dj6IxJdhQdVb4BQpZzdg3z8mXN4IEnWGadcTeS8PEE3rEEgefgpuWTFHvg/mCJ_NmkgeymFn4-7MznDX_mMEeFt1tEZHigLQw3d5ic",
+        avatar: `${geeks_url}/Anushavan-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/c/AnushavanYeghiazaryan",
             "facebook": "https://www.facebook.com/anushavan.yeghiazaryan24"
@@ -34,7 +36,7 @@ window.geeks = [
             "Interviews",
             "Podcasts"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/VZiZZ6TTHRyg6X2EbAI0NQ/aG9Uj28W7xHtrKqEK10QtMOX-hiACYEpWP0C0d1hEOI0XlaNBPBbDo3ENFFpDVPCFWM4Fk6A3qDYBNV7P8O7rQ/jIDSf4JPQopb5DPxJwLJwf8_nPSSz2Rjk8wEDUS3pZU",
+        avatar: `${geeks_url}/Nairi-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/c/nairihar",
             "medium": "https://nairihar.medium.com/",
@@ -49,7 +51,7 @@ window.geeks = [
             "Node.js",
             "Databases"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/wUJ1qYrFUfuA28mJ8A_bcQ/nuz-qWlFWBsv0HfVc6OhoAOOKzfD20VZgazGIuIqgXZq92sHAGFlYiDP5zZSFqxCKW6MzLo750kKDW1g2egO5w/m37GA1Cglw8631sNol2KfKcyiy6SxG6ehPLVkYhMysA",
+        avatar: `${geeks_url}/Rouben-min.jpg`,
         websites: {
             "youtube": "https://www.youtube.com/user/meschian",
             "facebook": "https://www.facebook.com/meschian/"
@@ -61,7 +63,7 @@ window.geeks = [
             "Canvas",
             "JavaScript"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/vZhBHrNB0pJKQHn88YaK5g/plhbktBQdqh6XB0d0BNCw2F1d9Um4zjCVIxou3cia12GPtRGSGg6ZKhYbufLNFMCHdoNnHgKYUKbPD_2eMMqPw/vKRRHEdtrHnMTtpzs-zS_cLGzEU1qKALiczy54K3wiM",
+        avatar: `${geeks_url}/Alen.png`,
         websites: {
             "youtube": "https://www.youtube.com/channel/UC3wj8tB4qIDH3u8_MHfbN8w",
             "medium": "https://medium.com/@AlenAbrahamyan",
@@ -74,7 +76,7 @@ window.geeks = [
             "Algorithms",
             "Data structures"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/7LCvWIoO836TNTi3ibUSXg/5NJllTWBuIIx294WiFsPTDU5qis4gVZr-CWmWHXP9r8kxhy7HJ6SnpETdY2scNv-nN8AsKYLl7TBhTxRMWE1Hg/mhAxmYEST-atBnVC80c1qKhoC-OnT8YJiDo6NZRL_pI",
+        avatar: `${geeks_url}/Tigran-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/channel/UCyW2YneDEtZj3QMTbsxKj9g"
         }
@@ -84,7 +86,7 @@ window.geeks = [
         hashtags: [
             "Podcasts"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/GEVP2vR8iTt81JD_V_2kww/CyRMSc4tujFhk4ZfGzJMIkyVMBokrJh2FRuQZ0C90tt9-E_nUefgtmofovh7x2QiWHssz195aaQwGlqmoK8YZA/1LHj0JYXQ8tE8fz9FeF_dIUNmi76_E2CjFjKNPNG7Ww",
+        avatar: `${geeks_url}/Davit-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/channel/UCzj2s7KYMpStMmnanYHkDmw",
             "facebook": "https://www.facebook.com/dbaghdasaryan"
@@ -95,7 +97,7 @@ window.geeks = [
         hashtags: [
             "Startups"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/KU6OH79n4soVYjzEE4qPwQ/1DXJ7-7J805zCcXFwy4Gcznl_j0ajBvUiwJnA-Lbk0Upzy8S9TLpFiJZhiae9MpSFXHjECmC_bs3xUFzqd-ozw/y9KTi9KA7DhmVOpfgdrAojASaaQCTwHiWCZo5yoVTW8",
+        avatar: `${geeks_url}/Narek-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/c/StartupGrowthArmenia/",
             "facebook": "https://www.facebook.com/ArmSnowboarder"
@@ -107,7 +109,7 @@ window.geeks = [
             "Machine Learning",
             "Podcasts"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/lkbZHwuMCoOqF_KWusCZDw/4qGFqhZmN5eb07yTAb0z49weelfZpE_2T7XS14aVAVFwhF5dyjsprzXu3xIXJe0ojbxNJ42SL2BBtYpU4GAerg/aE_N0BYw1LXfFwxMEzDIcDfjVtIzuhLQLChLxjVdDP0",
+        avatar: `${geeks_url}/Arthur-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/user/ArthurMuradyan"
         }
@@ -118,7 +120,7 @@ window.geeks = [
             "HTML/CSS",
             "Git"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/4dKZ26EVvWErOrHmRElevw/VTaEdpzGAkW9dML4nRTLK0vj8HoRNnl-OqvHiyg9GAe_N3rCfooo0xPBCCamnFMCSIkvkBfvQYZQJU9gVaBoBw/39uFS310WHGi0gI2oJ6cLByVubsu7zMN47DmqsiPh-o",
+        avatar: `${geeks_url}/Sami-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/c/SamiHayrapetyan",
             "facebook": "https://www.facebook.com/SamiHayrapetyanOfficial"
@@ -129,7 +131,7 @@ window.geeks = [
         hashtags: [
             "QA"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/UHeTg_3-4lZZWI6zV72Jdw/HX0E0Yp8ybLUFr9ttQT-24j8Z93tacAJ_RTRVnG4z9iXofSU8aqbLAz9oy1H60jBJr6m5bSMVzMGxQmzHJA-qg/bBQtD-aMR2Bv69hLQBeljdxGxFrEB3KFYjY1lk2RwHI",
+        avatar: `${geeks_url}/Narine-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/user/HayNariko",
             "facebook": "https://www.facebook.com/narinepoghosyan"
@@ -142,7 +144,7 @@ window.geeks = [
             "C-sharp",
             "SQL"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/9gLv1OMVgxLPbgRVN_-ksg/1tltTCuxWU6lttIOgrtwTwGXzpYh6DLYa1V2SP6ibF8zbaafwllyoP2fDAzBZoJsp9m5ViS2rkZLsuBVOyxydA/VHY7kM6-UYTOIb878gXmEawkgGni__0e4O0eXZJ4bNA",
+        avatar: `${geeks_url}/Seyran-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/c/NightCodeLive/playlists",
             "facebook": "https://www.facebook.com/seyran.tadevosyan.967"
@@ -153,7 +155,7 @@ window.geeks = [
         hashtags: [
             "DevOps"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/JTbMLVpG3vpUWP7cSBj1gw/6AKFYeNtxC_22uFJk9wtJSQLh8EA9CQkiWgz274pc0JFrbm_3YFXEePVqGp5WD5dWaBI_myk72O4IVcy2B8ZuQ/AYzGv4R6LEUj2kHO4s8Qyte1NkFrOIIzqMNz3UNZsy0",
+        avatar: `${geeks_url}/Grigor-min.jpeg`,
         websites: {
             "twitter": "https://twitter.com/grig_tech",
             "youtube": "https://www.youtube.com/c/GrigTech",
@@ -166,7 +168,7 @@ window.geeks = [
             "HTML/CSS",
             "JavaScript"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/-Gfqhk2VwkXiYJQeq1isug/9ZEcgQ7Yc5MN_8fPW00oCv1aCL5_8NOK244qpDhbViClSaiJfPyNkvHmS_pHI9jvJOmGM9AXJbscA1NiMHaDdQ/yrk8u2ZuA6aB2dL2vo4CxpgEDoOSdwNfrKdhJixzLvM",
+        avatar: `${geeks_url}/Andranik-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/channel/UCZXbYRualdbFzeNLPzBfjcQ",
             "facebook": "https://www.facebook.com/crueland"
@@ -177,7 +179,7 @@ window.geeks = [
         hashtags: [
             "HTML/CSS"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/H2QL05q3Leam1WksHjj0Vw/lXJG3ZEU_SqF030vBGS_wPceG_GKivoo7vFy1vaxUgQvmxhRARXNycWU-qR0hbmARKOTeKoA3gIaizB0oNNEtw/Fx2h-FD1eaRic8L-AzI2bs7J-IcCpbjjnxVx4xtFM-k",
+        avatar: `${geeks_url}/Amur-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/c/AmurKhoyetsyan",
             "facebook": "https://www.facebook.com/AmurKhoyetsyan"
@@ -190,7 +192,7 @@ window.geeks = [
             "Algorithms",
             "Data structures"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/jL-sO4Qnptnt5XhrAQI5Qw/Z2psD5pEXgPuxKeQcsMRvoEqg4vveA5ICnqEU62y4Jo1_gbHSZQdix6OZumfIvbPQztY51H1GbGVmIhL0qlwkg/KtJZCqlrn43UXZjkLbBiA-2l6rQJ_zqfPV49AF1K340",
+        avatar: `${geeks_url}/Arman-min.png`,
         websites: {
             "youtube": "https://www.youtube.com/c/CodeTubes",
             "facebook": "https://www.facebook.com/codetubesam/"
@@ -202,7 +204,7 @@ window.geeks = [
             "JavaScript",
             "React"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/maJyyRecKMIzb9KzWlp3Ug/LW9vPdSp-VxP-12JuQuW9pqzg5ycd8q-gTy2MGK6LPF1DPiWN-AqvJEiakCepuROoVv6aSukS7TdTMUG-BR4vw/VUqUMCeQBOl0z_Z045o86_9R2Eq_r7aum3FqoIgav-A",
+        avatar: `${geeks_url}/Edgar-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/channel/UCtdhQ5dsnKkd85d80IOzUvg",
             "facebook": "https://www.facebook.com/edd.marukyan.7"
@@ -213,7 +215,7 @@ window.geeks = [
         hashtags: [
             "Programming"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/bfRky3fZewxo5W7J3KDSqw/nSZT_vUjkqW7aOswDZfPQmwQGo42UMj488eelCFir6czpPa3kSF5E2uY1FYWBaf2wur-lksy0TMvSTpK4YoSMQ/X6ul5KECoCqQh-8xzy7hKvmzohXgJ-eUS5CcPynehs0",
+        avatar: `${geeks_url}/Kristine-min.jpg`,
         websites: {
             "medium": "https://medium.com/@christine.armiyants"
         }
@@ -224,7 +226,7 @@ window.geeks = [
             "Machine Learning",
             "GraphTheory"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/gsb6stmcZ_6-Xt-DFaOkng/V-KydU0cPAcAPa5beEY9j7WZNmxhUeIK4aceekqYqYOVu6CCQQe2mM2Dbnc0l4MG39lVp2aeV2HHuvo5mbMmaQ/pOxMLt2LFRmQWQf2vawf_ek_tUO1TPQ1junIS96VSPQ",
+        avatar: `${geeks_url}/Hrant-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/@HrantKhachatrian",
             "facebook": "https://www.facebook.com/hrant.khachatrian"
@@ -235,7 +237,7 @@ window.geeks = [
         hashtags: [
             "JavaScript"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/z-hTXBn69JGRTpMb074x5Q/b_SOMog2urmsBk9CULcIJQX8EepGPPEK8Wu0dVw3ZVjI-04_WhLGsrhqHbwh7DQ56DO2tqvfn71s1Y68j6Jb_A/pXE4i9goJlzWHkXM0D_cvo_0ulkTp1HcdsAFQwx9tR8",
+        avatar: `${geeks_url}/Ashot-min.jpg`,
         websites: {
             "youtube": "https://www.youtube.com/@iamashot",
             "medium": "https://medium.com/@iamashot"
@@ -247,7 +249,7 @@ window.geeks = [
             "Computer Science",
             "Algorithms"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/wN2wznAW76_e2Lw1cEq9Jg/F6bLmSsyxH1iiCusMurAzJPBUqbPhTjcS-FteQFdIiQAzpWqYkn-kZpsm36A8ydOu3z3lUcRRcUFan-E3UitaA/n8SYY3w6x6ZJbsBxVQ6tddeixft_-Um0t-gdvWfvCLc",
+        avatar: `${geeks_url}/Vahram-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/c/MutExSchool",
             "facebook": "https://www.facebook.com/vahram.martirosyan.7"
@@ -260,7 +262,7 @@ window.geeks = [
             "Algorithms",
             "Data structures"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/OXAfCxd-p2guPBeQtiVhCA/4LmVZ26y-gE_00MejWTZWvsmnE7alURv06y5blCQ1PnuqRACtKZx0kZT6VFF_SMk0l7nkFBPAyNIydELtBlXQA/ca0BU6dGoI0oHHoX7OOHb_xrDtnknTpqiMF_6eol6W8",
+        avatar: `${geeks_url}/Levon.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/@deimusmeister"
         }
@@ -271,7 +273,7 @@ window.geeks = [
             "Unix",
             "CyberSecurity"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/SFK3bhCSG08MLkNLIl4HkA/SELDrsLOY-vk6VHMjcm3Qdx2YvPxo77IcF9DCtiAskZ-3x1eURdqY9p3Wh3VB-4Q12jnRUChXENXOhHhbo2hSA/_EocuWLD902LXr2O8MOYD2xSiSgksAyhxAY4YfSGP-s",
+        avatar: `${geeks_url}/Antranig-min.jpeg`,
         websites: {
             "website": "https://weblog.antranigv.am"
         }
@@ -282,7 +284,7 @@ window.geeks = [
             "Java",
             "Programming"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/qJesd64ua8Tv9zUoeR4_yg/vQSmeLSXWI0Qi0hBQYkVA0VHIFYENMC3dj_YlqKUxF8w4Zd6ytQlThaiLO8WeFxtTLTD2xEfzwG7-xNTkGMQ_g/fC0lYnC6BpTJbgi6B-7CEm56RGVzAXJ3RaTafC02otU",
+        avatar: `${geeks_url}/Vahe-min.jpeg`,
         websites: {
             "medium": "https://medium.com/@vahe-avetikyan"
         }
@@ -293,7 +295,7 @@ window.geeks = [
             "Scratch",
             "Programming"
         ],
-        avatar: "https://v5.airtableusercontent.com/v1/15/15/1678204800000/j7Dq4c2X3Yv_ga9-3q0tmA/21oHmsS-Uw3Sj3PZCPulZCZtT0ilaluSTD-3qFEYLj5DV2Sa3669QIRO6RnNJU8itDcMSktBuzRizgxuKeIOVg/SbkePVmXjghcl7EUHaEXTFvPyj1Y9SR2sZAjt_gAtY0",
+        avatar: `${geeks_url}/Narek-min.jpeg`,
         websites: {
             "youtube": "https://www.youtube.com/@user-do6dv9tf5e"
         }
